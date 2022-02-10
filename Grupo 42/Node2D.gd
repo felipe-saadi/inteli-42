a90e069e134d6ca2114cd82970dc33851393752f
kathlyn diwan <kathlyn.diwan@sou.inteli.edu.br>
2022-02-10 15:27:27 -0300

    Update Node2D.gd
    Adicionei meu nome

--- Grupo 42/Node2D.gd
+++ Grupo 42/Node2D.gd
@@ -15,7 +15,7 @@ func _ready():
 func _on_Button_pressed():
 	if(show_authors == false):
 		show_authors = true
-		$Group_Authors.text = "Felipe Saadi"
+		$Group_Authors.text = "Felipe Saadi, Kathlyn Diwan"
 		$Button.text = "Esconder Autores"
 	else:
 		$Button.text = "Mostrar Autores"
